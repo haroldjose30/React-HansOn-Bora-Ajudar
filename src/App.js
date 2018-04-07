a526608af1d52ada0f950a5e42332172ea479048
Harold Jose Barros Gonçalves <haroldbarros@MacBook-Pro-de-Harold.local>
2018-04-07 04:18:02 -0300

    aula 02

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 import {
   BrowserRouter as Router,
-  Route    }
-from 'react-router-dom';
+  Route
+}
+  from 'react-router-dom';
 
 import Header from "./Header";
 import Footer from "./Footer";
@@ -12,31 +13,44 @@ import Sobre from "./Sobre";
 import Contato from "./Contato";
 import Campanhas from "./Campanhas";
 import base from "./base";
+import Admin from './Admin'
 
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
+    super(props)
     this.state = {
-      contador:1
+      contador: 1
     }
   }
 
-  componentDidMount(){
-    base.sy
-  } 
+
+
+  componentDidMount() {
+    base.syncState('contador', {
+      context: this,
+      state: 'contador',
+      asArray: false
+    })
+  }
 
   render() {
     return (
       <Router>
-      <div>
-        <Header/>  
-        <Route exact path='/' component={Home} />
-        <Route path='/Sobre' component={Sobre} />
-        <Route path='/Contato' component={Contato} />
-        <Route path='/Campanhas' component={Campanhas} />
-        <Footer/>
-      </div>
+        <div>
+          <Header />
+          <div>
+            <h1>Contador { this.state.contador} </h1>
+            <button onClick={()=> this.setState({contador:'ola react'})}>Ola </button>
+            <Route exact path='/' component={Home} />
+            <Route path='/Sobre' component={Sobre} />
+            <Route path='/Contato' component={Contato} />
+            <Route path='/Campanhas' component={Campanhas} />
+            <Route path='/Admin' component={Admin} />
+          </div>
+          <Footer />
+        </div>
       </Router>
     )
   }
